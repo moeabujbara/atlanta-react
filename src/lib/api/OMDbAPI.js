b9220ed6e5c31cf3d8cfdda4d8493be63dd94089
MJ <mh.abujbara@gmail.com>
2020-09-03 12:22:58 +0300

    changed http to https at api

--- src/lib/api/OMDbAPI.js
+++ src/lib/api/OMDbAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const gettheNameoftheMovieApi = (query) => {
-  return axios.get(" http://www.omdbapi.com/?i=tt3896198&apikey=f31abaef", {
+  return axios.get(" https://www.omdbapi.com/?i=tt3896198&apikey=f31abaef", {
     params: { s: query },
   });
 };
